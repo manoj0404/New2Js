9565e1615383998ce9b1c905c4e6bf6dc8b89ace
manoj0404 <manojpandharinathchaudhari@gmail.com>
2024-04-18 06:15:42 +0530

    initial commit

---
+++ Async.js
@@ -0,0 +1,12 @@
+/* Javascript : - synchronous
+                single threaded
+   Execution Content 
+                execute on line of code at a time
+                console log -1
+                console log -2
+
+                call stack         memory heap
+Blocking code vs                 non Blocking code
+block the flow of program        Does file Async
+ read file sync
+*/
